08c949999c42c074d376ecc8ae2a6aacd6a0248f
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-05-27 17:20:45 +0300

    Delete views.py

--- products/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# функции = контроллер = вьюхи = представления
-def index(request):
-    return render(request, 'products/index.html')
-
-def products(request):
-    return render(request, 'products/products.html')
