726a6d5894cb9132cbcd11554652e9645b0dd48d
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-07 13:09:06 +0300

    Add files via upload

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
-# Create your views here.
-# функции = контроллер = вьюхи = представления
+'''
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+'''
 
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
+# Create your views here.
+# функции = контроллер = вьюхи = представления
 def index(request):
     context = {
         'title' : 'GeekShop',
@@ -11,39 +18,22 @@ def index(request):
 
 def products(request):
     context = {
-        'products' : [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6090',
-             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни!'},
-            {'name': 'Синяя куртка The North Face', 'price': '23725',
-             'about': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель!'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3390',
-             'about': 'Материал с плюшевой текстурой. Удобный и мягкий!'},
-        ]
+        'title': 'GeekShop Products',
+        # 'categories': [
+        #     'Новинки',
+        #     'Одежда',
+        #     'Обувь',
+        #     'Аксессуары',
+        #     'Подарки',
+        # ],
+
     }
+    context['products'] = Product.objects.all()
+    print(Product.objects.all())
+
+
+    context['categories'] = ProductCategory.objects.all()
+    # print(ProductCategory.objects.all())
+    # print(ProductCategory.objects.get(id=1))
+
     return render(request, 'products/products.html', context)
-
-# Тестовая страница
-def test_context(request):
-    context = {
-        'title' : 'GeekShop',
-        'header' : 'Добро пожаловать на сайт',
-        'username' : ' Иван Иванов',
-    }
-    return render(request, 'products/test_context.html', context)
-
-# Тестовая страница
-def test(request):
-    context = {
-        'title' : 'GeekShop test',
-        'header' : 'Добро пожаловать на сайт',
-        'username' : ' Иван Иванов',
-        'products' : [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6090',
-             'about': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'name': 'Синяя куртка The North Face', 'price': '23725',
-             'about': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3390',
-             'about': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-        ]
-    }
-    return render(request, 'products/test.html', context)
