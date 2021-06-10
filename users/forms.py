546477004b429f6f8da99ebb4cafaa6f983481a0
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-10 15:19:53 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -29,3 +29,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        
