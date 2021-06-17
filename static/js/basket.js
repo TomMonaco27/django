a1ef7bf7c7b3413192682e5b91a77f86c78ba1ba
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-18 00:17:31 +0300

    Update basket.js

--- static/js/basket.js
+++ static/js/basket.js
@@ -4,7 +4,7 @@ window.onLoad = function () {
 	    $.ajax({
 	        url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
 	        success: function (data) {
-	            $.('.basket_list').html(data.result);
+	            $('.basket_list').html(data.result);
 	        }
 	    });
 	    event.preventDefault();
