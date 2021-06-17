3609927c509edad9ecbde5aa7ff4e7a95d16737c
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-17 15:54:15 +0300

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,16 +1,8 @@
-'''
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
-<!--                        <a class="nav-link" href="{% url 'users:login' %}">Войти <i class="fas fa-sign-in-alt"></i></a>-->
-'''
-
 from django.shortcuts import render
 
 from products.models import Product, ProductCategory
 
-# Create your views here.
-# функции = контроллер = вьюхи = представления
+
 def index(request):
     context = {
         'title' : 'GeekShop',
@@ -20,21 +12,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'GeekShop Products',
-        # 'categories': [
-        #     'Новинки',
-        #     'Одежда',
-        #     'Обувь',
-        #     'Аксессуары',
-        #     'Подарки',
-        # ],
-
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
-    context['products'] = Product.objects.all()
-    print(Product.objects.all())
-
-
-    context['categories'] = ProductCategory.objects.all()
-    # print(ProductCategory.objects.all())
-    # print(ProductCategory.objects.get(id=1))
-
     return render(request, 'products/products.html', context)
