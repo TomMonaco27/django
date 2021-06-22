9c249849edfc8f45f94c28664591d3f67fa4d1f0
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-22 11:31:38 +0300

    Update urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# для локальной работы с медиа-файлами
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -31,6 +31,6 @@ urlpatterns = [
 
 ]
 
-# Удостоверимся, что мы работаем локально
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
