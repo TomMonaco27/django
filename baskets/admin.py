ba17558fb623c5c3608589829f042f5ad944e99d
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-06-22 11:39:37 +0300

    Add files via upload

--- baskets/admin.py
+++ baskets/admin.py
@@ -1,3 +1,13 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from baskets.models import Basket
+
+
+#@admin.register(Basket)
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
+
